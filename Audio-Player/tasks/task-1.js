1085eed9e65616096d3b5ad755c26da4fc8197f2
pavelangelov <p_a_v_el@abv.bg>
2016-09-08 21:57:46 +0300

    Added solution for more of the tests.

--- Audio-Player/tasks/task-1.js
+++ Audio-Player/tasks/task-1.js
@@ -1,44 +1,302 @@
 function solve() {
+    let getId = function() {
+        let currentId = 0;
+        return function() {
+            return ++currentId;
+        };
+    }();
+
+    class Validator {
+        constructor() {}
+
+        static stringValidation(stringValue, paramName) {
+            const Min_String_Length = 3;
+            const Max_String_Length = 25;
+
+            if (!stringValue) {
+                throw new Error(`${paramName} cannot be empty!`);
+            }
+
+            let len = stringValue.length;
+            if (len < Min_String_Length || Max_String_Length < len) {
+                throw new Error(`${paramName} length must be between ${Min_String_Length} and ${Max_String_Length} symbols!`);
+            }
+        }
+
+        static checkForPlayListInstace(objToCheck) {
+            if (!(objToCheck instanceof PlayList)) {
+                throw new Error('Cannot add object different from PlayList!');
+            }
+        }
+
+        static checkForPlayableInstance(item) {
+            let playableKeys = Object.keys(Playable);
+            for (key of playableKeys) {
+                if (!item[key]) {
+                    throw new Error('Cannot add item different from playable!');
+                }
+            }
+            // if(!(item instanceof Playable)) {
+            //     throw new Error('Cannot add item different from playable!');
+            // }
+        }
+
+        static checkAudioLength(audioLength) {
+            if (audioLength <= 0) {
+                throw new Error('The lenght of the audio must be greater than 0!');
+            }
+        }
+
+        static validateMediaRating(imdbRating) {
+            const Min_Rating_Value = 1;
+            const Max_Rating_Value = 5;
+
+            if (imdbRating < Min_Rating_Value || Max_Rating_Value < imdbRating) {
+                throw new Error(`Video rating must be between ${Min_Rating_Value} and ${Max_Rating_Value}.`);
+            }
+        }
+    }
+
+    class Player {
+        constructor(name) {
+            this.name = name;
+            this._id = getId();
+            this._playLists = [];
+        }
+
+        get name() {
+            return this._name;
+        }
+
+        set name(value) {
+            Validator.stringValidation(value);
+
+            this._name = value;
+        }
+
+        get id() {
+            return this._id;
+        }
+
+        addPlaylist(playlistToAdd) {
+            Validator.checkForPlayListInstace(playlistToAdd);
+
+            this._playLists.push(playlistToAdd);
+
+            return this;
+        }
+
+        getPlaylistById(id) {
+            var playList = null;
+
+            this._playLists.forEach(function(list) {
+                if (list.id === id) {
+                    playList = list;
+                }
+            });
+
+            return playList;
+        }
+
+        removePlaylist(option) {
+            let id;
+            if (typeof option === 'object') {
+                id = option.id;
+            } else if (typeof option === 'number') {
+                id = option;
+            }
+
+            let found = false;
+            for (let i = 0, len = this._playLists.length; i < len; i += 1) {
+                let currentElement = this._playLists[i];
+                if (currentElement.id === id) {
+                    found = true;
+                    this._playLists.splice(i, 1);
+                    break;
+                }
+            }
+
+            if (!found) {
+                throw new Error('Playlist with this id does not exist!');
+            }
+
+            return this;
+        }
+    }
+
+    class PlayList {
+        constructor(name) {
+            this.name = name;
+            this.id = getId();
+            this._playableItems = [];
+        }
+
+        get name() {
+            return this._name;
+        }
+
+        set name(value) {
+            Validator.stringValidation(value);
+
+            this._name = value;
+        }
+
+        addPlayable(playable) {
+            Validator.checkForPlayableInstance(playable);
+
+            this._playableItems.push(playable);
+
+            return this;
+        }
+
+        getPlayableById(id) {
+            let playable = null;
+
+            for (let i = 0, len = this._playableItems.length; i < len; i += 1) {
+                let currentItem = this._playableItems[i];
+                if (currentItem.id === id) {
+                    playable = this._playableItems.splice(i, 1)[0];
+                    break;
+                }
+            }
+
+            return playable;
+        }
+
+        removePlayable(options) {
+            let id,
+                found = false;
+
+            if (typeof options === 'number') {
+                id = options;
+            } else if (typeof options === 'object') {
+                id = options.id;
+            }
+
+            for (let i = 0, len = this._playableItems.length; i < len; i += 1) {
+                let currentItem = this._playableItems[i];
+                if (currentItem.id === id) {
+                    found = true;
+                    this._playableItems.splice(i, 1);
+                    break;
+                }
+            }
+
+            if (!found) {
+                throw new Error('Item with this id does not exist!');
+            }
+        }
+
+        listPlayables(page, size) {
+            // TODO
+        }
+    }
+
+    class Playable {
+        constructor(title, author) {
+            this.title = title;
+            this.author = author;
+            this._id = getId();
+        }
+
+        get title() {
+            return this._title;
+        }
+
+        set title(value) {
+            Validator.stringValidation(value);
+
+            this._title = value;
+        }
+
+        get author() {
+            return this._author;
+        }
+
+        set author(value) {
+            Validator.stringValidation(value);
+
+            this._author = value;
+        }
+
+        get id() {
+            return this._id;
+        }
+
+        play() {
+            return `${id}. ${title} - ${author}`;
+        }
+    }
+
+    class Audio extends Playable {
+        constructor(title, author, length) {
+            super(title, author);
+            this.length = length;
+        }
+
+        get length() {
+            return this._length;
+        }
+
+        set length(value) {
+            Validator.checkAudioLength(value);
+
+            this._length = value;
+        }
+
+        play() {
+            let baseOutput = super.play();
+
+            return `${baseOutput} - ${this.length}`;
+        }
+    }
+
+    class Video extends Playable {
+        constructor(title, author, imdbRating) {
+            super(title, author);
+            this.imdbRating = imdbRating;
+        }
+
+        get imdbRating() {
+            return this._imdbRating;
+        }
+
+        set imdbRating(value) {
+            Validator.validateMediaRating(value);
+
+            this._imdbRating = value;
+        }
+
+        play() {
+            let baseOutput = super.play();
+
+            return `${baseOutput} - ${this.imdbRating}`;
+        }
+    }
+
     return {
         getPlayer: function (name) {
-            
+            return new Player(name);
         },
         getPlaylist: function (name) {
-           
+           return new PlayList(name);
         },
         getAudio: function (title, author, length) {
-            
+            return new Audio(title, author, length);
         },
         getVideo: function (title, author, imdbRating) {
-            
+            return new Video(title, author, imdbRating);
         }
     };
 }
+var returnedPlayable,
+    name = 'Rock and roll',
+    playlist = solve().getPlaylist(name),
+    playable = { id: 1, name: 'Banana Rock', author: 'Wombles' };
 
-// var result = solve();
-// var audio = result.getAudio('asd', 'sdf', 4);
-// audio = result.getAudio('asd', 'sdf', 4);
-// console.log(audio.play());
-
-/*var pl = result.getPlaylist('asd');
-
-var playable = {id: 1, name: 'Rock', author: 'Stephen'};
-pl.addPlayable(playable);
-console.log(pl.getPlayableById(1));
-
-console.log(pl.listPlayables(0, 10));
-pl.removePlayable(1);
-console.log(pl.getPlayableById(1));
-
-var list = result.getPlaylist('Rock');
-for (var i = 0; i < 35; i += 1) {
-    list.addPlayable({id: (i + 1), name: 'Rock' + (9 - (i % 10))});
-}
-
-//console.log(list.listPlayables(0, 10));
-
-/*returnedPlayables = list.listPlaylables(2,10);
- returnedPlayables = list.listPlaylables(3,10);
- console.log(returnedPlayables);*/
+// returnedPlayable = playlist.addPlayable(playable).getPlayableById(1);
+// console.log(returnedPlayable.id);
+// console.log(returnedPlayable.name);
+// console.log(returnedPlayable.author);
 
 module.exports = solve;
